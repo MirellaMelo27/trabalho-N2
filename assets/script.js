6a9563d65d670daeddca1413d6c664e367d73c49
Karen <karenamancio.dev@gmail.com>
2025-05-04 14:43:51 -0300

    inclusao da pasta assets e organizacao dos modulos - inclui o js para funcionar a pagina lateral

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+function openMenu() {
+    document.getElementById("sidebar").style.width = "250px"; // Aumenta a largura do menu lateral para 250px
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.width = "0"; // Diminui a largura do menu lateral para 0 (fecha o menu)
+}
+
